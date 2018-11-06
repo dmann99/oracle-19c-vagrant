fc839aee861d02ec42ce7f225cc32be687b99266
David Mann <david.mann@sas.com>
2018-11-06 13:50:43 -0500

    Updated install.sh

--- 19/scripts/install.sh
+++ 19/scripts/install.sh
@@ -4,9 +4,10 @@ echo 'INSTALLER: Start'
 STARTDATE=`date`
 echo ''
 
-# Get VM packages up to date
-echo 'INSTALLER: Yum Upgrade All'
-yum upgrade -y
+# get up to date
+#echo 'INSTALLER: Yum Upgrade All'
+#yum upgrade -y
+#yum reinstall -y glibc-common
 echo ''
 
 
@@ -14,37 +15,41 @@ echo ''
 echo 'INSTALLER: Fix Locale Warning'
 echo LANG=en_US.utf-8 >> /etc/environment
 echo LC_ALL=en_US.utf-8 >> /etc/environment
+echo 'INSTALLER: Locale set'
 echo ''
 
 # install Oracle Database prereq packages
-echo 'INSTALLER: Oracle preinstall .RPMs'
+echo 'INSTALLER: Oracle preinstall started...'
 yum install dejavu-serif-fonts
 yum install -y oracle-database-preinstall-18c
+echo 'INSTALLER: Oracle preinstall complete'
 echo ''
 
 # set environment variables
-echo 'INSTALLER: Environment variables set in .bashrc'
+echo 'INSTALLER: Environment variables set in .bashrc start'
 echo "export ORACLE_BASE=$ORACLE_BASE" >> /home/oracle/.bashrc && \
 echo "export ORACLE_HOME=$ORACLE_HOME" >> /home/oracle/.bashrc && \
 echo "export ORACLE_SID=$ORACLE_SID" >> /home/oracle/.bashrc   && \
 echo "export PATH=\$PATH:\$ORACLE_HOME/bin" >> /home/oracle/.bashrc
+echo 'INSTALLER: Environment variables set complete'
 echo ''
 
-echo 'INSTALLER: Oracle directory creation'
+echo 'INSTALLER: Oracle directory creation start'
 mkdir -p $ORACLE_BASE
 chown oracle:oinstall -R $ORACLE_BASE
 mkdir -p $ORACLE_HOME
 chown oracle:oinstall -R $ORACLE_HOME
-mkdir -p $ORACLE_INVENTORY
-chown oracle:oinstall -R $ORACLE_INVENTORY
+mkdir -p $ORACLE_BASE/oraInventory
+chown oracle:oinstall -R $ORACLE_BASE/oraInventory
 mkdir -p $ORACLE_BASE/oradata
 chown oracle:oinstall -R $ORACLE_BASE/oradata
 #usermod -g dba oracle
 echo ''
 
-# Extend Swap Space using /dev/sdb extra disk supplied with the VM
+
 echo 'INSTALLER: Increasing Swap Space'
-sfdisk /dev/sdb < /vagrant/scripts/sdb.layout
+# Extend Swap Space using /dev/sdb supplied with the VM
+sfdisk /dev/sdb < /vagrant/sdb.layout
 pvcreate /dev/sdb1
 vgextend vg_main /dev/sdb1
 lvextend /dev/vg_main/lv_swap /dev/sdb1
@@ -52,45 +57,30 @@ swapoff -v /dev/vg_main/lv_swap
 mkswap /dev/vg_main/lv_swap
 swapon -va
 swapon -s
-echo ''
 
 # Unzip Install Package
 echo 'INSTALLER: Unzip Installation Package'
-su -l oracle -c 'unzip /vagrant/V978967*.zip -d ${ORACLE_HOME}'
-su -l oracle -c 'unzip /vagrant/LINUX*18*.zip -d ${ORACLE_HOME}'
+su -l oracle -c 'unzip /vagrant/191000*.zip -d ${ORACLE_HOME}'
 echo ''
 
-echo 'INSTALLER: Oracle software install'
+echo 'INSTALLER: Oracle software install start'
 su -l oracle -c "/vagrant/scripts/go-dbinstall.sh"
-$ORACLE_INVENTORY/orainstRoot.sh
+
+
+$ORACLE_HOME/oraInventory/orainstRoot.sh
 $ORACLE_HOME/root.sh
-echo ''
 
-echo 'INSTALLER: Create CDB/PDB'
+echo 'INSTALLER: Create CDB/PDB Start'
 su -l oracle -c "/vagrant/scripts/go-dbcreate.sh"
-su -l oracle -c "sqlplus / as sysdba <<EOF
-   ALTER PLUGGABLE DATABASE $ORACLE_PDB SAVE STATE;
-   exit;
-EOF"
-echo ''
+echo 'INSTALLER: Create CDB/PDB Complete'
 
-echo 'INSTALLER: Create Listener'
-echo 'INSTALLER: Listener install start'
+echo 'INSTALLER: Create Listener Start'
 su -l oracle -c "netca -silent -responseFile /vagrant/ora-response/netca_typ.rsp"
+echo 'INSTALLER: Create Listener Complete'
 echo ''
 
 
-echo 'INSTALLER: Oratab configure /etc/oratab'
-sed '$s/N/Y/' /etc/oratab | sudo tee /etc/oratab > /dev/null
-
-## configure systemd to start oracle instance on startup
-#sudo cp /vagrant/scripts/oracle-rdbms.service /etc/systemd/system/
-#sudo sed -i -e "s|###ORACLE_HOME###|$ORACLE_HOME|g" /etc/systemd/system/oracle-rdbms.service
-#sudo systemctl daemon-reload
-#sudo systemctl enable oracle-rdbms
-#sudo systemctl start oracle-rdbms
-#echo "INSTALLER: Created and enabled oracle-rdbms systemd's service"
-
-echo 'INSTALLER: Installation complete'
+echo 'INSTALLER: Installation complete, check for errors, database should be ready to use!'
 echo Start Time  : $STARTDATE
 echo Finish Time : `date`
+
